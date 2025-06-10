cc8f1a844510267884da8586bcd23967c2914149
sophiaAlbuquerque <sophiaalbuquerqueeleuterio@gmail.com>
2025-06-10 16:46:19 -0300

    Update package.json to include gh-pages dependency and adjust scripts for deployment. Modify routes to use HashRouter for better routing compatibility. Update vite.config.js to ensure correct base path for deployment.

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,14 +5,14 @@ import Gallery from "./pages/gallery";
 
 function AppRoutes() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
-          <Route path="/produtos" element={<Gallery />} />
+          <Route path="produtos" element={<Gallery />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
